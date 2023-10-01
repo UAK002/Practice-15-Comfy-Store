68e224755b1ed9016a9a433f315604da88a94064
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-02 01:36:06 +0530

    R03

--- 0 Day/3/src/pages/Login.jsx
+++ 0 Day/3/src/pages/Login.jsx
@@ -1,5 +1,52 @@
+import { Form, Link } from 'react-router-dom';
+
+import { FormInput, SubmitBtn } from '../components';
+
 const Login = () => {
-  // log(something);
-  return <div>Login</div>;
+  return (
+    <section className="grid place-items-center  h-screen">
+      {/* (Hs,G, Pic ) (Hs,G, Pic) h-screen, grid, place-items-center*/}
+      {/*  (C,W,P, Bg, Sha, F, F-c, G) card, w-96, p-8,
+       bg-base-100, shadow-lg, flex, flex-col, gap-y-4 */}
+
+      {/* (Tc, T-3, Fb ) text-center, text-3xl, font-bold */}
+
+      {/*  (Ml, L, L-h, L-P, C )ml-2 link link-hover 
+        link-primary capitalize */}
+      <Form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <FormInput
+          label="Email"
+          type="email"
+          name="identifier"
+          defaultValue="test@test.com"
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          name="password"
+          defaultValue="secret"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="login" />
+        </div>
+        <button type="button" className="btn btn-block btn-secondary">
+          Guest User
+        </button>
+        <p className="text-center">
+          Not a member yet?{' '}
+          <Link
+            to="/register"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            register
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 export default Login;

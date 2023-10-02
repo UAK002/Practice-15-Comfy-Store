bef18dbf8c5238449ac2dacad60421c65ca7178f
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-03 03:38:53 +0530

    R02

--- 0 Day/2/src/components/Header.jsx
+++ 0 Day/2/src/components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
       <div className="align-element flex justify-center sm:justify-end">
         {/* USER */}
         {/* LINKS */}
-        <div className="flex gap-x-2 justify-center items-center">
+        <div className="flex gap-x-6 justify-center items-center">
           <Link to="/login" className="link link-hover text-xs sm:text-sm">
-            Register
+            Sign in / Guest
           </Link>
           <Link to="/login" className="link link-hover text-xs sm:text-sm">
-            Register
+            Create Account
           </Link>
         </div>
       </div>
@@ -19,11 +19,3 @@ const Header = () => {
   );
 };
 export default Header;
-
-// /- G header tags class= 3 (bg, p, t-n-c )bg-neutral & py-2 & text-neutral-content
-// - Header > div class= (a-e, f, j-c, sm:j-e)align-element & flex justify-content & sm:justify-end
-// - W div > comment USER
-// - W div > comment LINKS
-//  W div > under LINKS comment > div class =( F, G-x, j-c, i-c ) flex, gap-x-6 & justify-center &
-// - W div.flex > render Link to="/login" class=(L, L-h, T-x, sm: T-s ) link & link-hover & text-xs & sm:text-sm > {Sign in / Guest }
-// - W div.flex > render Link to="/register" class=(L, L-h, T-x, sm: T-s ) link & link-hover & text-xs & sm:text-sm > {Create Account }

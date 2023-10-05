9fed948181146d4b4639229dc3f2b8a6dc62a2be
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-05 14:54:52 +0530

    r01

--- 0 Day/1/src/components/Navbar.jsx
+++ 0 Day/1/src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(false);
+
+  const handleTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element">
@@ -35,6 +42,13 @@ const Navbar = () => {
         </div>
         <div className="navbar-end">
           {/* THEME  */}
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            {/* Sun Icon */}
+            <BsSunFill className="swap-on w-4 h-4" />
+            {/* Moon Icon */}
+            <BsMoonFill className="swap-off w-4 h-4" />
+          </label>
           {/* CART */}
           <NavLink to="/cart" className="btn btn-ghost btn-circle, btn-md ml-4">
             <div className="indicator">

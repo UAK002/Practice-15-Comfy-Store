6674ca542c3e19df0e090a6cd4a08d92c04e0cee
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-05 16:41:15 +0530

    R01

--- 0 Day/1/src/components/Navbar.jsx
+++ 0 Day/1/src/components/Navbar.jsx
@@ -2,14 +2,31 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+};
+
+const getThemeFromLocalStorage = () => {
+  localStorage.getItem('Theme') || themes.winter;
+};
 
 const Navbar = () => {
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
 
   const handleTheme = () => {
-    setTheme(!theme);
+    const { winter, dracula } = themes;
+    const newTheme = theme === winter ? dracula : winter;
+    // document.documentElement.setAttribute('data-theme', theme);
+    setTheme(newTheme);
   };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (
     <nav className="bg-base-200">

50dc268107ff41d0d9ae00dcbc0ad9215d88625e
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-17 22:40:51 +0530

    R02

--- 0 Day/2/src/App.jsx
+++ 0 Day/2/src/App.jsx
@@ -18,6 +18,7 @@ import { ErrorElement } from './components';
 
 // Loader
 import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
 
 // Actions
 
@@ -40,6 +41,8 @@ const router = createBrowserRouter([
       {
         path: 'products/:id',
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
       },
       {
         path: 'about',
--- 0 Day/2/src/components/ProductsGrid.jsx
+++ 0 Day/2/src/components/ProductsGrid.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, Link } from 'react-router-dom';
+import { formatPrice } from '../utils';
 
 const ProductsGrid = () => {
   const { products } = useLoaderData();
@@ -7,10 +8,11 @@ const ProductsGrid = () => {
     <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
       {products.map((product) => {
         const { title, image, price } = product.attributes;
+        const dollarsAmount = formatPrice(price);
         return (
           <Link
             key={product.id}
-            to={`/product/${product.id}`}
+            to={`/products/${product.id}`}
             className=" card w-full shadow-xl hover:shadow-2xl transition duration-300"
           >
             <figure className="px-4 pt-4">
@@ -22,7 +24,7 @@ const ProductsGrid = () => {
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title capitalize tracking-wider">{title}</h2>
-              <span className="text-secondary">{price}</span>
+              <span className="text-secondary">{dollarsAmount}</span>
             </div>
           </Link>
         );

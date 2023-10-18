561f7fc7cf54f1808cb2a770047e74dbda480f1c
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-18 19:01:41 +0530

    R01

--- 0 Day/1/src/pages/SingleProduct.jsx
+++ 0 Day/1/src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData, Link } from 'react-router-dom';
-import { customFetch, formatPrice } from '../utils';
+import { customFetch, formatPrice, generateAmountOptions } from '../utils';
 import { useState } from 'react';
 
 export const loader = async ({ params }) => {
@@ -82,9 +82,7 @@ const SingleProduct = () => {
               className="select select-secondary select-bordered select-md"
               onChange={handleAmount}
             >
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
+              {generateAmountOptions(20)}
             </select>
           </div>
           {/* CART */}

1a19d515719d93e9f4f3da806a1df136933b659b
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-20 22:18:17 +0530

    R3

--- R3/src/pages/HomeLayout.jsx
+++ R3/src/pages/HomeLayout.jsx
@@ -1,4 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
 const HomeLayout = () => {
-  return <div>HomeLayout</div>;
+  return (
+    <>
+      <nav className="text-4xl text-primary">comfy</nav>
+      <Outlet />
+    </>
+  );
 };
 export default HomeLayout;
--- R3/src/pages/Login.jsx
+++ R3/src/pages/Login.jsx
@@ -1,4 +1,46 @@
+import { Link, Form } from 'react-router-dom';
+import { FormInput, SubmitBtn } from '../components';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className=" h-screen grid place-items-center">
+      <Form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <FormInput
+          type="email"
+          label="email"
+          name="identifier"
+          defaultValue="test@test.com"
+        />
+        <FormInput
+          type="password"
+          label="password"
+          name="password"
+          defaultValue="secret"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="login" />
+        </div>
+        <button type="button" className="btn btn-secondary btn-block">
+          Guest
+        </button>
+        <p className="text-center">
+          Not a member yet?
+          <Link
+            to="/register"
+            className="ml-2 link link-hover link-primary capitalize "
+          >
+            register
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 export default Login;
+
+// - W Form > p class=  > { Not a member yet?}
+// - W p tags after the content > render Link comp. to="/register" class= 5(Ml, L, L-h, L-P, C )ml-2 link link-hover link-primary capitalize > { register}

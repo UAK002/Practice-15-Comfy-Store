a24aecd22df61d4bbcaf7e6b3a6777cecfbd0b82
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-21 19:57:10 +0530

    R05

--- 0 Day/5/src/components/Filters.jsx
+++ 0 Day/5/src/components/Filters.jsx
@@ -1,4 +1,46 @@
+import { useLoaderData, Form, Link } from 'react-router-dom';
+import FormInput from './FormInput';
+import FormSelect from './FormSelect';
+import FormRange from './FormRange';
+
 const Filters = () => {
-  return <div>Filters</div>;
+  const { meta } = useLoaderData();
+  // console.log(meta);
+  return (
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* SEARCH */}
+      <FormInput type="search" label="search" name="search" size="input-sm" />
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        size="select-sm"
+        list={meta.categories}
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        size="select-sm"
+        list={meta.companies}
+      />
+      {/* ORDER */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        size="select-sm"
+        list={['a-z', 'z-a', 'High', 'Low']}
+      />
+      {/* PRICE */}
+      <FormRange label="select price" name="price" size="range-sm" />
+      {/* BUTTONS */}
+      <button type="submit" className="btn btn-primary btn-sm">
+        search
+      </button>
+      <Link to="/products" className="btn btn-accent btn-sm">
+        reset
+      </Link>
+    </Form>
+  );
 };
 export default Filters;

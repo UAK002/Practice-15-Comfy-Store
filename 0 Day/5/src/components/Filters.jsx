564079e5556b5a57694fa784a130750261f1b252
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-10-23 19:22:44 +0530

    R05

--- 0 Day/5/src/components/Filters.jsx
+++ 0 Day/5/src/components/Filters.jsx
@@ -2,20 +2,29 @@ import { useLoaderData, Form, Link } from 'react-router-dom';
 import FormInput from './FormInput';
 import FormSelect from './FormSelect';
 import FormRange from './FormRange';
+import FormCheckbox from './FormCheckbox';
 
 const Filters = () => {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
   // console.log(meta);
+  const { search, category, company, order, price, shipping } = params;
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
-      <FormInput type="search" label="search" name="search" size="input-sm" />
+      <FormInput
+        type="search"
+        label="search"
+        name="search"
+        size="input-sm"
+        defaultValue={search}
+      />
       {/* CATEGORIES */}
       <FormSelect
         label="select category"
         name="category"
         size="select-sm"
         list={meta.categories}
+        defaultValue={category}
       />
       {/* COMPANIES */}
       <FormSelect
@@ -23,6 +32,7 @@ const Filters = () => {
         name="company"
         size="select-sm"
         list={meta.companies}
+        defaultValue={company}
       />
       {/* ORDER */}
       <FormSelect
@@ -30,11 +40,28 @@ const Filters = () => {
         name="order"
         size="select-sm"
         list={['a-z', 'z-a', 'High', 'Low']}
+        defaultValue={order}
       />
       {/* PRICE */}
-      <FormRange label="select price" name="price" size="range-sm" />
+      <FormRange
+        label="select price"
+        name="price"
+        size="range-sm"
+        price={price}
+      />
+      {/* SHIPPING */}
+      <FormCheckbox
+        label="free-shipping"
+        name="shipping"
+        size="checkbox-sm"
+        defaultValue={shipping}
+      />
       {/* BUTTONS */}
-      <button type="submit" className="btn btn-primary btn-sm">
+      <button
+        type="submit"
+        className="btn btn-primary btn-sm"
+        defaultValue={category}
+      >
         search
       </button>
       <Link to="/products" className="btn btn-accent btn-sm">

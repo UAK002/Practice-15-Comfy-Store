ff7612cfd45c0df7fb4e118cda7308892387956d
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-16 19:30:57 +0530

    R1

---
+++ R1/src/components/SubmitBtn.jsx
@@ -0,0 +1,8 @@
+const SubmitBtn = ({ text }) => {
+  return (
+    <button className="btn btn-primary" type="submit">
+      {text}
+    </button>
+  );
+};
+export default SubmitBtn;
--- R1/src/pages/Login.jsx
+++ R1/src/pages/Login.jsx
@@ -1,5 +1,43 @@
+import { Form, Link } from 'react-router-dom';
+import { FormInput, SubmitBtn } from '../components';
+
 const Login = () => {
-  // console.log(something);
-  return <div>Login</div>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        action="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <FormInput
+          type="text"
+          name="identifier"
+          label="Email"
+          defaultValue="test@test.com"
+        />
+        <FormInput
+          type="password"
+          name="password"
+          label="Password"
+          defaultValue="secret"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="Login" />
+        </div>
+        <button type="button" className="btn btn-secondary btn-block">
+          Guest
+        </button>
+        <p className="text-center">
+          not a member yet?
+          <Link
+            to="/register"
+            className="ml-2 link link-hover link-primary capitalize "
+          >
+            Register
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 export default Login;

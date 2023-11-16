d9d680a340390f4658e7b9014c0126aa544c6ccf
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-16 18:50:45 +0530

    R1

--- R1/src/App.jsx
+++ R1/src/App.jsx
@@ -9,10 +9,60 @@ import {
   Orders,
   Products,
   Register,
-  SingleProductPage,
+  SingleProduct,
 } from './pages';
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 };
 export default App;
--- R1/src/pages/Error.jsx
+++ R1/src/pages/Error.jsx
@@ -1,4 +1,32 @@
+import { useRouteError, Link } from 'react-router-dom';
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <main className="min-h-[100vh] grid place-items-center px-8">
+        <div className="text-center">
+          <h1 className="text-9xl font-semibold text-primary ">404</h1>
+          <h4 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            page not found
+          </h4>
+          <p className="mt-6 text-lg leading-7">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-10">
+            <Link to="/" className="btn btn-secondary">
+              Go Back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main className="min-h-[100vh] grid place-items-center px-8">
+      <h4 className="text-4xl text-center font-bold">There was an error...</h4>
+    </main>
+  );
 };
 export default Error;

67c298e7035d13ab99d278f30886978530a88a4a
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-16 19:37:14 +0530

    R1

--- R1/src/components/SubmitBtn.jsx
+++ R1/src/components/SubmitBtn.jsx
@@ -1,7 +1,23 @@
+import { useNavigation } from 'react-router-dom';
+
 const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  //   console.log(navigation);
+  const isSubmitting = navigation.state === 'submitting';
   return (
-    <button className="btn btn-primary" type="submit">
-      {text}
+    <button
+      className="btn btn-primary btn-block"
+      type="submit"
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner"></span>
+          sending...
+        </>
+      ) : (
+        text || 'submit'
+      )}
     </button>
   );
 };
--- R1/src/pages/Error.jsx
+++ R1/src/pages/Error.jsx
@@ -2,7 +2,7 @@ import { useRouteError, Link } from 'react-router-dom';
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+  // console.log(error);
   if (error.status === 404) {
     return (
       <main className="min-h-[100vh] grid place-items-center px-8">

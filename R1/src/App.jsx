98b320dc8eba5289e360e9877db3535367c8772f
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-16 13:48:17 +0530

    r0000

--- R1/src/App.jsx
+++ R1/src/App.jsx
@@ -1,80 +1,35 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import {
-  About,
-  Cart,
-  Checkout,
-  Error,
-  HomeLayout,
-  Landing,
-  Login,
-  Orders,
-  Products,
-  Register,
-  SingleProduct,
-} from './pages';
+function App() {
+  const [count, setCount] = useState(0)
 
-import { ErrorElement } from './components';
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-// Loaders
-import { loader as landingLoader } from './pages/Landing';
-import { loader as singleProductLoader } from './pages/SingleProduct';
-
-// Actions
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomeLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-        errorElement: <ErrorElement />,
-        loader: landingLoader,
-      },
-      {
-        path: 'products',
-        element: <Products />,
-      },
-      {
-        path: 'products/:id',
-        element: <SingleProduct />,
-        errorElement: <ErrorElement />,
-        loader: singleProductLoader,
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-      {
-        path: 'cart',
-        element: <Cart />,
-      },
-      {
-        path: 'checkout',
-        element: <Checkout />,
-      },
-      {
-        path: 'orders',
-        element: <Orders />,
-      },
-    ],
-  },
-  {
-    path: '/login',
-    element: <Login />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
-    errorElement: <Error />,
-  },
-]);
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
-export default App;
+export default App
--- R3/src/components/SubmitBtn.jsx
+++
@@ -1,8 +0,0 @@
-const SubmitBtn = ({ text }) => {
-  return (
-    <button type="submit" className="btn btn-primary">
-      {text}
-    </button>
-  );
-};
-export default SubmitBtn;

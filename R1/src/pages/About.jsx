80be35d855c99c11cf603b5734d8c428d7616a9c
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-17 17:55:19 +0530

    R1

--- R1/src/pages/About.jsx
+++ R1/src/pages/About.jsx
@@ -1,4 +1,28 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className=" flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl ">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest  ">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        aspernatur consequatur ullam commodi animi nostrum impedit ut aliquam
+        necessitatibus quisquam dolore quod quaerat, nesciunt corporis non nam
+        quam quos, voluptate incidunt eveniet. Eos reprehenderit dignissimos
+        illum harum ducimus fugit in?
+      </p>
+    </>
+  );
 };
 export default About;
+
+// - W React fragment > p 5(M-6, T-L, L-8, M-W-2 M-A ) class=  > {Lorem 40 }

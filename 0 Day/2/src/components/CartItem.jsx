c0dbe39b8d3a1bf9afbe8f6f47f139fff198cd8b
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-20 15:20:38 +0530

    R02

--- 0 Day/2/src/components/CartItem.jsx
+++ 0 Day/2/src/components/CartItem.jsx
@@ -3,6 +3,15 @@ import { removeItem, editItem } from '../features/cart/cartSlice';
 import { useDispatch } from 'react-redux';
 
 const CartItem = ({ cartItem }) => {
+  const dispatch = useDispatch();
+
+  const removeItemFromTheCart = () => {
+    dispatch(removeItem({ cartID }));
+  };
+  const handleAmount = (e) => {
+    dispatch(editItem({ cartID, amount: parseInt(e.target.value) }));
+  };
+
   const { cartID, image, title, company, productColor, amount, price } =
     cartItem;
   return (
@@ -33,7 +42,7 @@ const CartItem = ({ cartItem }) => {
           ></span>
         </p>
       </div>
-      <div className="sm:ml-24">
+      <div className="sm:ml-12">
         {/* AMOUNT */}
         <div className="form-control max-w-xs">
           <label htmlFor="amount" className="label p-0">
@@ -43,6 +52,8 @@ const CartItem = ({ cartItem }) => {
             name="amount"
             id="amount"
             className="mt-2 select select-base select-bordered select-xs "
+            value={amount}
+            onChange={handleAmount}
           >
             {generateAmountOption(amount + 5)}
           </select>
@@ -51,6 +62,7 @@ const CartItem = ({ cartItem }) => {
         <button
           type="button"
           className=" mt-2 link link-primary link-hover text-sm "
+          onClick={removeItemFromTheCart}
         >
           remove
         </button>

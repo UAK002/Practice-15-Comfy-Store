8cad132018065ec9670d6517ad1d2d80c720a5a5
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-20 12:56:52 +0530

    R1

--- R1/src/components/CartItem.jsx
+++ R1/src/components/CartItem.jsx
@@ -1,4 +1,57 @@
-const CartItem = () => {
-  return <div>cartItem</div>;
+import { Link } from 'react-router-dom';
+import { formatPrice, generateAmountOptions } from '../utils';
+import { useDispatch } from 'react-redux';
+
+const CartItem = ({ cartItem }) => {
+  const { cartID, image, title, company, productColor, amount, price } =
+    cartItem;
+  return (
+    <article
+      key={cartID}
+      className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0"
+    >
+      {/* IMAGE */}
+      <img
+        src={image}
+        alt=""
+        className="h-24 w-24 rounded-lg sm:h-32 sm-w-32 object-cover"
+      />
+      {/* INFO */}
+      <div className="sm:ml-16 sm:w-48">
+        <h3 className="capitalize font-medium">{title}</h3>
+        <h4 className="mt-2 capitalize text-sm text-neutral-content">
+          {company}
+        </h4>
+        <p className="mt-4 text-sm capitalize flex items-center gap-x-2">
+          color:{' '}
+          <span
+            className="badge badge-sm"
+            style={{ background: productColor }}
+          ></span>
+        </p>
+      </div>
+      <div className="sm:ml-24">
+        {/* AMOUNT */}
+        <div className="from-control max-w-xs">
+          <label htmlFor="amount" className="label p-0">
+            <span className="label-text">Amount</span>
+          </label>
+          <select
+            name="amount"
+            id="amount"
+            className="mt-2 select select-base select-bordered select-xs"
+          >
+            {generateAmountOptions(amount + 5)}
+          </select>
+        </div>
+        {/* REMOVE */}
+        <button className="mt-2 link link-primary link-hover text-sm ">
+          remove
+        </button>
+      </div>
+      {/* PRICE */}
+      <p className="font-medium sm:ml-auto">{formatPrice(price)}</p>
+    </article>
+  );
 };
 export default CartItem;

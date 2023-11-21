7ed118574902a02aee88576d1a3501d2401b4a61
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-21 16:06:41 +0530

    R01

--- 0 Day/1/src/components/Header.jsx
+++ 0 Day/1/src/components/Header.jsx
@@ -1,26 +1,45 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../Features/cart/cartSlice';
+import { logoutUser } from '../Features/user/userSlice';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userState.user);
+
+  const handleLogout = () => {
+    navigate('/');
+    dispatch(clearCart());
+    dispatch(logoutUser());
+  };
+
   return (
     <header className="bg-neutral py-2 text-neutral-content">
-      {/*  class=(bg, p, t-n-c )bg-neutral & py-2 & text-neutral-content
-       class= (a-e, f, j-c, sm:j-e)align-element & flex justify-content & sm:justify-end
-         class =( F, G-x, j-c, i-c ) flex, gap-x-6 & justify-center & items-center
-         class=(L, L-h, T-x, sm: T-s ) link & link-hover & text-xs & sm:text-sm
- 
-      
-      */}
       <div className="align-element flex justify-center sm:justify-end">
+        {user ? (
+          <div className="flex gap-x-2 sm:gap-x-8 items-center">
+            <p className="text-xs sm-text-sm">Hello, {user.username}</p>
+            <button
+              className=" btn btn-xs btn-outline btn-primary"
+              onClick={handleLogout}
+            >
+              logout
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-x-6 justify-center items-center">
+            <Link to="/login" className="link link-hover text-xs sm:text-sm">
+              Sign in / Guest
+            </Link>
+            <Link to="/register" className="link link-hover text-xs sm:text-sm">
+              Register
+            </Link>
+          </div>
+        )}
+
         {/* USER */}
         {/* LINKS */}
-        <div className="flex gap-x-6 justify-center items-center">
-          <Link to="/login" className="link link-hover text-xs sm:text-sm">
-            Sign in / Guest
-          </Link>
-          <Link to="/register" className="link link-hover text-xs sm:text-sm">
-            Register
-          </Link>
-        </div>
       </div>
     </header>
   );

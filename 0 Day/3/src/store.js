437fe295b50b7f7efe7b17ccb081f958ad334ddb
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-11-21 18:24:58 +0530

    R03

--- 0 Day/3/src/store.js
+++ 0 Day/3/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './Features/cart/cartSlice';
+import userReducer from './Features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     cartState: cartReducer,
+    userState: userReducer,
   },
 });

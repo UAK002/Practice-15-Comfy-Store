aad63c628b706075f2c3bb19dcec8791cb08aa6a
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-12-09 18:33:27 +0530

    R01

--- 0Day/1/src/pages/Checkout.jsx
+++ 0Day/1/src/pages/Checkout.jsx
@@ -1,5 +1,18 @@
 import { useSelector } from 'react-redux';
 import { CheckoutForm, SectionTitle, CartTotals } from '../components';
+import { redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+export const loader = (store) => () => {
+  const user = store.getState().userState.user;
+  // console.log(user);
+  if (!user) {
+    toast.warn('You must be logged in to check out');
+    // return redirect('/login');
+    return redirect('/');
+  }
+  return null;
+};
 
 const Checkout = () => {
   const cartTotal = useSelector((state) => state.cartState.cartTotal);
